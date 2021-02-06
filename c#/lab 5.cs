c2c3883ed82a6aa26c3f505d72f5e113b4a33fdf
nazzyn <59263031+nazzyn@users.noreply.github.com>
2021-02-06 20:14:37 +0200

    Add files via upload

---
+++ c#/lab 5.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            WMatrix min = new WMatrix();
+            var mat = min.CreatMatrix();
+            WMatrix max = new WMatrix();
+            max.MatrixMin(mat);
+            max.SortMatrix(mat);
+        }
+    }
+}
---
+++ c#/lab 6.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReverseMethod reverse = new ReverseMethod();
+            reverse.NumRevers();
+            reverse.NumRevers(1234);
+            Console.WriteLine(ReverseString());
+            reverse.ReverseString();
+            Console.WriteLine(reverse.ReverseNumber(123.456)); 
+            reverse.ReverseNumber();
+             reverse.ReverseWord();
+             reverse.ReverseWord("Hello my frined, how are you?");
+            Console.WriteLine(reverse.DontReverse("Test text")); 
+            reverse.DontReverse();
+            string text = "Hello Danya!";
+             reverse.DontReverse(ref text); 
+            string test = "Test";
+            reverse.DontReverse(test, out string texts);
+
+        }
+        static string ReverseString()
+        {
+            string word = "ABCD";
+            char[] array = word.ToCharArray();
+            Array.Reverse(array);
+            return new string(array);
+        }
+
+    }
+}
